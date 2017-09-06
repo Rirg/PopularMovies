e05f0cbbdb9cb683c031158f40b537746d32ad78
Ricardo Ruiz Giler <Ricardo@Ricardos-MacBook-Pro.local>
2017-09-06 18:50:37 -0500

    Create codes for the paths, a UriMatcher method and implement the insert method in the provider class.

--- app/src/main/java/com/example/ricardo/popularmovies/data/FavoritesContentProvider.java
+++ app/src/main/java/com/example/ricardo/popularmovies/data/FavoritesContentProvider.java
@@ -1,11 +1,16 @@
 package com.example.ricardo.popularmovies.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import com.example.ricardo.popularmovies.data.FavoritesMoviesContract.FavoriteMoviesEntry;
 
 /**
  * Created by Ricardo on 9/6/17.
@@ -13,12 +18,30 @@ import android.support.annotation.Nullable;
 
 public class FavoritesContentProvider extends ContentProvider{
 
+    public static final int FAVORITES = 100;
+    public static final int FAVORITE_WITH_ID = 101;
+
+    private FavoritesMoviesDbHelper mFavoritesDbHelper;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(FavoritesMoviesContract.AUTHORITY, FavoritesMoviesContract.PATH_FAVORITES,
+                FAVORITES);
+        uriMatcher.addURI(FavoritesMoviesContract.AUTHORITY, FavoritesMoviesContract.PATH_FAVORITES
+                + "/#", FAVORITE_WITH_ID);
+
+        return uriMatcher;
+    }
 
 
 
     @Override
     public boolean onCreate() {
-        return false;
+        mFavoritesDbHelper = new FavoritesMoviesDbHelper(getContext());
+        return true;
     }
 
     @Nullable
@@ -36,7 +59,27 @@ public class FavoritesContentProvider extends ContentProvider{
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+
+        final SQLiteDatabase db = mFavoritesDbHelper.getWritableDatabase();
+
+        int code = sUriMatcher.match(uri);
+        Uri returnedUri;
+
+        switch (code) {
+            case FAVORITES:
+                long id = db.insert(FavoriteMoviesEntry.TABLE_NAME, null, contentValues);
+                if (id > 0) {
+                    returnedUri = ContentUris.withAppendedId(FavoriteMoviesEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown Uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        
+        return returnedUri;
     }
 
     @Override
